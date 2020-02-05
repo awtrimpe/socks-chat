f391146f5676dd69f352c04f99b3ba3872b15a63
Alex Trimpe <alexwtrimpe@gmail.com>
2020-02-05 14:36:48 -0500

    Tests created for all routes

--- tests/app/main/routes/test_routes.py
+++ tests/app/main/routes/test_routes.py
@@ -1,5 +1,6 @@
 from flask import url_for
 
+from app.main.database.tables import User
 from app.main.users import register_user
 
 
@@ -55,3 +56,49 @@ def describe_about():
     def test_about_page(client):
         resp = client.get('/about')
         assert b'<title>Socks Chat | About</title>' in resp.data
+
+
+def describe_register():
+    def test_register_page(client):
+        resp = client.get('/register')
+        assert b'<h2>Create Account</h2>' in resp.data
+
+    def test_register_user(client, session):
+        data = {'username': 'sabmiller', 'password': 'ColdAsTheRockies',
+                'password_conf': 'ColdAsTheRockies', 'first_name': 'Coors', 'last_name': 'Light'}
+        resp = client.post('/register', data=data, follow_redirects=True)
+        assert resp.status_code == 200
+        assert b'<title>Socks Chat | Home</title>' in resp.data
+        with session() as session:
+            user = session.query(User).filter_by(username='sabmiller').first()
+            assert user.first_name == 'Coors'
+
+    def test_invalid_user_input(client):
+        data = {'username': 'sabmiller', 'password': 'ColdAsTheRockies',
+                'first_name': 'Coors', 'last_name': 'Light'}
+        resp = client.post('/register', data=data, follow_redirects=True)
+        assert b'Not all required fields provided' in resp.data
+
+    def test_non_matching_passwords(client):
+        data = {'username': 'sabmiller', 'password': 'ColdAsTheRockies',
+                'password_conf': 'ReachForTheCold', 'first_name': 'Coors', 'last_name': 'Light'}
+        resp = client.post('/register', data=data, follow_redirects=True)
+        assert b'Passwords did not match' in resp.data
+
+    def test_user_already_registered(client, session):
+        with session() as session:
+            new_user = register_user(
+                session, 'sabmiller', 'ColdAsTheRockies', 'Coors', 'Light')
+            session.add(new_user)
+            session.commit()
+            data = {'username': 'sabmiller', 'password': 'ColdAsTheRockies',
+                    'password_conf': 'ColdAsTheRockies', 'first_name': 'Coors', 'last_name': 'Light'}
+            resp = client.post('/register', data=data, follow_redirects=True)
+            assert b'Username already used' in resp.data
+
+
+def describe_logout():
+    def test_logout(client):
+        resp = client.get('/logout', follow_redirects=True)
+        assert b'<title>Socks Chat | Home</title>' in resp.data
+        assert b'You have been successfully logged out' in resp.data
