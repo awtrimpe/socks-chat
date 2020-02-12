3ac0feb2d18fb3f4e4b1595b6cc743e423db92b2
Alex Trimpe <alexwtrimpe@gmail.com>
2020-02-11 20:45:07 -0500

    Pinpointing failing test

--- tests/app/main/users/test_users.py
+++ tests/app/main/users/test_users.py
@@ -4,36 +4,35 @@ from flask import current_app, request, url_for
 from app.main.database.tables import User
 from app.main.users import get_user, register_user
 
+# def describe_register_user():
+#     def test_register_unique(session):
+#         with session() as session:
+#             resp = register_user(session,
+#                                  'cmorgan', 'LiveLikeTheCaptain', 'Captain', 'Morgan')
+#             assert type(resp) == User
 
-def describe_register_user():
-    def test_register_unique(session):
-        with session() as session:
-            resp = register_user(session,
-                                 'cmorgan', 'LiveLikeTheCaptain', 'Captain', 'Morgan')
-            assert type(resp) == User
+#     def test_register_already_exists(session):
+#         with session() as session:
+#             with pytest.raises(Exception) as exc:
+#                 new_user = register_user(
+#                     session, 'cmorgan', 'LiveLikeTheCaptain', 'Captain', 'Morgan')
+#                 session.add(new_user)
+#                 session.commit()
+#                 register_user(session, 'cmorgan',
+#                               'LiveLikeTheCaptain', 'Captain', 'Morgan')
+#         assert str(exc.value) == 'Username already used'
 
-    def test_register_already_exists(session):
-        with session() as session:
-            with pytest.raises(Exception) as exc:
-                new_user = register_user(
-                    session, 'cmorgan', 'LiveLikeTheCaptain', 'Captain', 'Morgan')
-                session.add(new_user)
-                session.commit()
-                register_user(session, 'cmorgan',
-                              'LiveLikeTheCaptain', 'Captain', 'Morgan')
-        assert str(exc.value) == 'Username already used'
-
-    def test_password_hashed(session):
-        with session() as session:
-            new_user = register_user(
-                session, 'cmorgan', 'LiveLikeTheCaptain', 'Captain', 'Morgan')
-            session.add(new_user)
-            session.commit()
-            user = session.query(User).filter_by(username='cmorgan').first()
-            assert user.first_name == 'Captain'
-            assert user.last_name == 'Morgan'
-            assert user.username == 'cmorgan'
-            assert user.password != 'LiveLikeTheCaptain'
+#     def test_password_hashed(session):
+#         with session() as session:
+#             new_user = register_user(
+#                 session, 'cmorgan', 'LiveLikeTheCaptain', 'Captain', 'Morgan')
+#             session.add(new_user)
+#             session.commit()
+#             user = session.query(User).filter_by(username='cmorgan').first()
+#             assert user.first_name == 'Captain'
+#             assert user.last_name == 'Morgan'
+#             assert user.username == 'cmorgan'
+#             assert user.password != 'LiveLikeTheCaptain'
 
 
 def describe_get_user():
